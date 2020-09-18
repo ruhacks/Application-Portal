499e964087036e4146d9342a0c4153a5c84b056c
Dan Stingaciu <stingaciu.sebastian@gmail.com>
2020-09-18 19:07:13 -0400

    Adding redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import InitalComp from './js/components/initalComp';
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
+
+const store = configureStore();
 
 const App = () => {
-    return <InitalComp />;
+    return (
+        <Provider store={store}>
+            <InitalComp />
+        </Provider>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('container'));
