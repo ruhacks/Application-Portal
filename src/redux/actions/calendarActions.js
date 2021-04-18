f043870cf38ca4aa46ad3e33e138cbc7919a6ecf
Dan Stingaciu <dsstingaciu@gmail.com>
2021-04-18 01:02:29 -0400

    Added get commands for calendar
    getAllCalenderInfo()
    - Gets the whole calendar in the following format:
    
    calendar : {
        DAY_ONE: {
            <event_ID>: {
                ...event_data
            }
        }
        ...
    }
    
    getEventsForDay(day)
    - Gets all events for one day in the following format:
    
    dayEvents : {
        <event_ID>: {...event_data}
    }
    
    getOneEvent(day, event_ID)
    - Gets event information for specific day

--- src/redux/actions/calendarActions.js
+++ src/redux/actions/calendarActions.js
@@ -12,9 +12,81 @@ export const CALENDAR_DELETE_EVENT_REQUEST = "CALENDAR_DELETE_EVENT_REQUEST";
 export const CALENDAR_DELETE_EVENT_SUCCESS = "CALENDAR_DELETE_EVENT_SUCCESS";
 export const CALENDAR_DELETE_EVENT_FAILURE = "CALENDAR_DELETE_EVENT_FAILURE";
 
+export const CALENDAR_GET_ALL_REQUEST = "CALENDAR_GET_REQUEST";
+export const CALENDAR_GET_ALL_SUCCESS = "CALENDAR_GET_SUCCESS";
+export const CALENDAR_GET_ALL_FAILURE = "CALENDAR_GET_FAILURE";
+
+export const CALENDAR_EVENT_GET_REQUEST = "CALENDAR_EVENT_GET_REQUEST";
+export const CALENDAR_EVENT_GET_SUCCESS = "CALENDAR_EVENT_GET_SUCCESS";
+export const CALENDAR_EVENT_GET_FAILURE = "CALENDAR_EVENT_GET_FAILURE";
+
+export const CALENDAR_GET_DAY_REQUEST = "CALENDAR_GET_DAY_REQUEST";
+export const CALENDAR_GET_DAY_SUCCESS = "CALENDAR_GET_DAY_SUCCESS";
+export const CALENDAR_GET_DAY_FAILURE = "CALENDAR_GET_DAY_FAILURE";
+
 export const DAY_FRI = "DAY_FRI";
 export const DAY_SUN = "DAY_SUN";
 export const DAY_SAT = "DAY_SAT";
+
+export const getDayReq = () => {
+    return {
+        type: CALENDAR_GET_DAY_REQUEST,
+    };
+};
+
+export const getDaySuccess = (dayEvents) => {
+    return {
+        type: CALENDAR_GET_DAY_SUCCESS,
+        dayEvents,
+    };
+};
+
+export const getDayError = (error) => {
+    return {
+        type: CALENDAR_GET_DAY_FAILURE,
+        error,
+    };
+};
+
+export const getEventReq = () => {
+    return {
+        type: CALENDAR_EVENT_GET_REQUEST,
+    };
+};
+
+export const getEventSuccess = (event) => {
+    return {
+        type: CALENDAR_EVENT_GET_SUCCESS,
+        event,
+    };
+};
+
+export const getEventError = (error) => {
+    return {
+        tyoe: CALENDAR_EVENT_GET_FAILURE,
+        error,
+    };
+};
+
+export const getAllCalReq = () => {
+    return {
+        type: CALENDAR_GET_ALL_REQUEST,
+    };
+};
+
+export const getAllCalSuccess = (calendar) => {
+    return {
+        type: CALENDAR_GET_ALL_SUCCESS,
+        calendar,
+    };
+};
+
+export const getAllCalFailure = (error) => {
+    return {
+        type: CALENDAR_GET_ALL_FAILURE,
+        error,
+    };
+};
 
 export const createEventReq = () => {
     return {
@@ -73,6 +145,152 @@ export const deleteEventFailure = (error) => {
         type: CALENDAR_DELETE_EVENT_FAILURE,
         error,
     };
+};
+
+export const getOneEvent = (day, event_ID) => async (dispatch) => {
+    dispatch(getEventReq());
+
+    let databaseDay;
+    switch (day) {
+        case "DAY_FRI":
+            databaseDay = "DAY_ONE";
+            break;
+        case "DAY_SAT":
+            databaseDay = "DAY_TWO";
+            break;
+        case "DAY_SUN":
+            databaseDay = "DAY_THREE";
+            break;
+        default:
+            return dispatch(
+                getEventError({
+                    message: "Error! Invalid day provided",
+                    eventEdited: false,
+                })
+            );
+    }
+    let EVENT_SNAP;
+
+    const EVENT_REF = firestore.doc(
+        `calendar/${databaseDay}/events/${event_ID}`
+    );
+
+    try {
+        EVENT_SNAP = await EVENT_REF.get();
+    } catch (error) {
+        dispatch(getEventError(error));
+    }
+
+    if (EVENT_SNAP.exists) {
+        dispatch(getEventSuccess(EVENT_SNAP.data()));
+    } else {
+        dispatch(getEventSuccess({}));
+    }
+};
+
+export const getEventsForDay = (day) => async (dispatch) => {
+    dispatch(getDayReq());
+
+    let databaseDay;
+
+    switch (day) {
+        case "DAY_FRI":
+            databaseDay = "DAY_ONE";
+            break;
+        case "DAY_SAT":
+            databaseDay = "DAY_TWO";
+            break;
+        case "DAY_SUN":
+            databaseDay = "DAY_THREE";
+            break;
+        default:
+            return dispatch(
+                editEventFailure({
+                    message: "Error! Invalid day provided",
+                    eventEdited: false,
+                })
+            );
+    }
+
+    const DAY_EVENTS_REF = firestore.collection(
+        `calendar/${databaseDay}/events`
+    );
+    let DAY_EVENTS_SNAP;
+    try {
+        DAY_EVENTS_SNAP = await DAY_EVENTS_REF.get();
+    } catch (error) {
+        dispatch(getDayError(error));
+    }
+
+    const DAY_OBJ = {};
+
+    if (!DAY_EVENTS_SNAP.empty) {
+        DAY_EVENTS_SNAP.forEach((eventDoc) => {
+            DAY_OBJ[eventDoc.id] = eventDoc.data();
+        });
+    }
+
+    dispatch(getDaySuccess(DAY_OBJ));
+};
+
+export const getAllCalendarInfo = () => async (dispatch) => {
+    dispatch(getAllCalReq());
+
+    const calendar = {};
+
+    const DAY_ONE_EVENTS = firestore.collection(`calendar/DAY_ONE/events`);
+    const DAY_TWO_EVENTS = firestore.collection("calendar/DAY_TWO/events");
+    const DAY_THREE_EVENTS = firestore.collection("calendar/DAY_THREE/events");
+
+    let DAY_ONE_SNAPS;
+    let DAY_TWO_SNAPS;
+    let DAY_THREE_SNAPS;
+
+    try {
+        DAY_ONE_SNAPS = await DAY_ONE_EVENTS.get();
+    } catch (error) {
+        dispatch(getAllCalFailure(error));
+    }
+
+    try {
+        DAY_TWO_SNAPS = await DAY_TWO_EVENTS.get();
+    } catch (error) {
+        dispatch(getAllCalFailure(error));
+    }
+
+    try {
+        DAY_THREE_SNAPS = await DAY_THREE_EVENTS.get();
+    } catch (error) {
+        dispatch(getAllCalFailure(error));
+    }
+
+    const DAY_FRI = {};
+    const DAY_SAT = {};
+    const DAY_SUN = {};
+
+    if (!DAY_ONE_SNAPS.empty) {
+        DAY_ONE_SNAPS.forEach((eventDoc) => {
+            DAY_FRI[eventDoc.id] = eventDoc.data();
+        });
+    }
+
+    if (!DAY_TWO_SNAPS.empty) {
+        DAY_TWO_SNAPS.forEach((eventDoc) => {
+            DAY_SAT[eventDoc.id] = eventDoc.data();
+        });
+    }
+
+    if (!DAY_THREE_SNAPS.empty) {
+        DAY_THREE_SNAPS.forEach((eventDoc) => {
+            DAY_SUN[eventDoc.id] = eventDoc.data();
+        });
+    }
+
+    calendar["DAY_FRI"] = DAY_FRI;
+    calendar["DAY_SAT"] = DAY_SAT;
+    calendar["DAY_SUN"] = DAY_SUN;
+
+    dispatch(getAllCalSuccess(calendar));
 };
 
 export const createEvent = (day, info) => async (dispatch) => {
--- src/redux/reducers/calendar.js
+++ src/redux/reducers/calendar.js
@@ -8,10 +8,31 @@ import {
     CALENDAR_DELETE_EVENT_REQUEST,
     CALENDAR_DELETE_EVENT_SUCCESS,
     CALENDAR_DELETE_EVENT_FAILURE,
+    CALENDAR_GET_ALL_REQUEST,
+    CALENDAR_GET_ALL_SUCCESS,
+    CALENDAR_GET_ALL_FAILURE,
+    CALENDAR_EVENT_GET_REQUEST,
+    CALENDAR_EVENT_GET_SUCCESS,
+    CALENDAR_EVENT_GET_FAILURE,
+    CALENDAR_GET_DAY_REQUEST,
+    CALENDAR_GET_DAY_SUCCESS,
+    CALENDAR_GET_DAY_FAILURE,
 } from "../actions/calendarActions";
 
 export default (
     state = {
+        getSpecificEvent: false,
+        specificEvent: {},
+        getSpecificEventError: {},
+
+        getDayEvents: false,
+        dayEvents: {},
+        gettingDayEventsError: {},
+
+        gettingFullCalendar: false,
+        calendar: {},
+        getinngFullCalendarError: {},
+
         creatingEvent: false,
         createEventError: {},
         createdEventID: null,
@@ -26,6 +47,57 @@ export default (
     action
 ) => {
     switch (action.type) {
+        case CALENDAR_EVENT_GET_REQUEST:
+            return {
+                ...state,
+                getSpecificEvent: true,
+            };
+        case CALENDAR_EVENT_GET_SUCCESS:
+            return {
+                ...state,
+                getSpecificEvent: false,
+                specificEvent: action.event,
+            };
+        case CALENDAR_EVENT_GET_FAILURE:
+            return {
+                ...state,
+                getSpecificEvent: false,
+                getSpecificEvent: action.error,
+            };
+        case CALENDAR_GET_DAY_REQUEST:
+            return {
+                ...state,
+                getDayEvents: true,
+            };
+        case CALENDAR_GET_DAY_SUCCESS:
+            return {
+                ...state,
+                getDayEvents: false,
+                dayEvents: action.dayEvents,
+            };
+        case CALENDAR_GET_DAY_FAILURE:
+            return {
+                ...state,
+                getDayEvents: false,
+                gettingDayEventsError: action.error,
+            };
+        case CALENDAR_GET_ALL_REQUEST:
+            return {
+                ...state,
+                gettingFullCalendar: true,
+            };
+        case CALENDAR_GET_ALL_SUCCESS:
+            return {
+                ...state,
+                gettingFullCalendar: false,
+                calendar: action.calendar,
+            };
+        case CALENDAR_GET_ALL_FAILURE:
+            return {
+                ...state,
+                gettingFullCalendar: false,
+                gettingFullCalendarError: action.error,
+            };
         case CALENDAR_CREATE_EVENT_REQUEST:
             return {
                 ...state,
